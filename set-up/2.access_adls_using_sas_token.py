51044f83fc3167db79f98c3dba9f17fa8aac85cf
gautham-sn <learnings15223@gmail.com>
2024-09-18 12:01:27 +0530

    Update 2.access_adls_using_sas_token.py

--- set-up/2.access_adls_using_sas_token.py
+++ set-up/2.access_adls_using_sas_token.py
@@ -1,7 +1,6 @@
 # Databricks notebook source
 spark.conf.set("fs.azure.account.auth.type.formula1dl15.dfs.core.windows.net", "SAS")
 spark.conf.set("fs.azure.sas.token.provider.type.formula1dl15.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-spark.conf.set("fs.azure.sas.fixed.token.formula1dl15.dfs.core.windows.net", "sp=rl&st=2023-03-23T11:54:11Z&se=2023-03-23T19:54:11Z&spr=https&sv=2021-12-02&sr=c&sig=avmlTVD6ZqJInYSEQaLWj8gXZj6qsYZOhmIVb4Dg9tg%3D")
 
 # COMMAND ----------
 
